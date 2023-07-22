c5700d1eda75af9f99f1decbea2752159a726ce6
mirenecarmen <ca.ir.ma.80@gmail.com>
2023-07-21 23:49:14 -0300

    Agrego tabla-objeto Colores y corregir nombre de entidades a singular

--- src/clasePruebaRamaNueva/colores_pruebas.sql
+++ src/clasePruebaRamaNueva/colores_pruebas.sql
@@ -1,9 +1,15 @@
-CREATE TABLE colores (
+CREATE TABLE color (
     id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     codigo_hex VARCHAR(7) NOT NULL
 );
-INSERT INTO colores (id, nombre, codigo_hex) VALUES
+CREATE TABLE objeto (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    id_color INT,
+    FOREIGN KEY (id_color) REFERENCES color(id)
+);
+INSERT INTO color (id, nombre, codigo_hex) VALUES
 (1, 'Rojo', '#FF0000'),
 (2, 'Verde', '#00FF00'),
 (3, 'Azul', '#0000FF');
